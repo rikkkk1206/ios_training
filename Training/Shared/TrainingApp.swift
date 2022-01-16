fa24706dd17f3fd7f193cf1ade711899940c3914
RIKU <riku@RIKUnoMacBook-Pro.local>
2022-01-16 17:50:38 +0900

    プロジェクト作成、YumemiWeather導入

---
+++ Training/Shared/TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingApp.swift
+//  Shared
+//
+//  Created by RIKU on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
